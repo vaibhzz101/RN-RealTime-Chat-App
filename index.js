8e63a32b8358965d7e9e299bfd61a546a2f0d6a0
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-06-26 15:06:30 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+
+
+const port = process.env.PORT || 7070;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
